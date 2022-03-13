ad79a0e563353c4e281be5739b5fe257d7d80fd1
squirrels <squirrelinthewoods@protonmail.com>
2022-03-13 16:29:23 +0200

    Added functionality to start page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstRiddleComponent } from './first-riddle/first-riddle.component';
+import { StartComponent } from './start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'start', component: StartComponent, pathMatch: 'full'},
+  {path: 'first-riddle',  component: FirstRiddleComponent},  
+  {path: '', pathMatch: 'full', redirectTo: 'start'},
+  {path: '**', redirectTo: 'posts', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  hasPageBeenClicked = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onPageClick() {
+    this.hasPageBeenClicked = true;
+  }
+
+  onGetStarted() {
+    this.router.navigateByUrl('/first-riddle');
   }
 
 }
